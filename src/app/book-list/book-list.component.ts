1666eb1eae75c34c90ef377f983752a3543fa9f2
Daniel Drion <daniel.drion@telenet.be>
2025-01-16 14:49:35 +0100

    Using the BookService in the BookList component and subscribe to it

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Book } from '../book';
 import { CommonModule } from '@angular/common';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-list',
@@ -9,10 +10,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css',
 })
-export class BookListComponent {
-  books: Book[] = [
-    { id: 1, title: 'Book one', author: 'Author one' },
-    { id: 2, title: 'Book two', author: 'Author two' },
-    { id: 3, title: 'Book three', author: 'Author three' },
-  ];
+export class BookListComponent implements OnInit {
+  books: Book[] = [];
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    this.bookService.getBooks().subscribe((books) => (this.books = books));
+  }
 }
